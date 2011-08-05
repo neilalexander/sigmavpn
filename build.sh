5dd6cdf9c3eb1cfd3b786e8c9e06d48a0676b2db
Neil Alexander <neilalexander@frozenriver.net>
2011-08-05 20:16:35 +0100

    Fixed the build script so that it does not try to compile the nacl protocol anymore when the headers are not in the right location

--- build.sh
+++ build.sh
@@ -13,7 +13,10 @@ gcc 	$CCFLAGS -c 		-o build/main.o main.c
 gcc	$CCFLAGS -c 		-o build/modules.o modules.c
 gcc 	$CCFLAGS -c 		-o build/types.o types.c
 gcc 	$CCFLAGS $DYFLAGS 	-o build/proto_raw.o proto/proto_raw.c
-gcc 	$CCFLAGS $DYFLAGS 	-o build/proto_nacl0.o proto/proto_nacl0.c lib/libnacl.a
+if [ -f "./include/crypto_box_curve25519xsalsa20poly1305.h" ]
+then
+        gcc     $CCFLAGS $DYFLAGS       -o build/proto_nacl0.o proto/proto_nacl0.c lib/libnacl.a
+fi
 gcc 	$CCFLAGS $DYFLAGS 	-o build/intf_dummy.o intf/intf_dummy.c
 gcc 	$CCFLAGS $DYFLAGS 	-o build/intf_tuntap.o intf/intf_tuntap.c
 gcc 	$CCFLAGS $DYFLAGS 	-o build/intf_udp.o intf/intf_udp.c
