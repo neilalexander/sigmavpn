53685179ff0a862a5a3d3c5a9cb5a003a6e5d745
Neil Alexander <neilalexander@frozenriver.net>
2011-08-06 21:08:38 +0100

    Updated the build script to build nacl on demand, and link against it for nacl0 protocol if available

--- build.sh
+++ build.sh
@@ -14,12 +14,43 @@ gcc	$CCFLAGS -c 		-o build/modules.o modules.c
 gcc 	$CCFLAGS -c 		-o build/types.o types.c
 gcc	$CCFLAGS -c		-o build/ini.o dep/ini.c
 gcc 	$CCFLAGS $DYFLAGS 	-o build/proto_raw.o proto/proto_raw.c
+
+if [ ! -f "./include/crypto_box_curve25519xsalsa20poly1305.h" ]
+then
+	if [ ! "$1" == "--with-nacl" ]
+	then
+		echo The nacl encryption library has not been found, so features relying on nacl will not be built.
+		echo If you wish to build nacl also, re-run build.sh with the --with-nacl parameter.
+	fi
+fi
+
+if [ "$1" == "--with-nacl" ]
+then
+	echo "Building nacl; this will take a while..."
+	rm -rf tmp/
+	mkdir tmp
+	mkdir tmp/nacl
+	cd tmp/nacl
+	curl -O -q http://hyperelliptic.org/nacl/nacl-20110221.tar.bz2
+	bunzip2 -d nacl-20110221.tar.bz2
+	tar -xf nacl-20110221.tar --strip-components 1
+	./do
+	cd ../../
+	ABI=`tmp/nacl/build/*/bin/okabi | head -n 1`
+	HOST=`hostname -s`
+	mkdir lib/
+	mkdir include/
+	cp -r "tmp/nacl/build/${HOST}/lib/${ABI}/" lib/
+	cp -r "tmp/nacl/build/${HOST}/include/${ABI}/" include/
+fi
+
 if [ -f "./include/crypto_box_curve25519xsalsa20poly1305.h" ]
 then
         gcc     $CCFLAGS $DYFLAGS       -o build/proto_nacl0.o proto/proto_nacl0.c lib/libnacl.a build/types.o
 	gcc	$CCFLAGS -c		-o build/naclkeypair.o naclkeypair.c
 	gcc	$CCFLAGS		-o build/naclkeypair build/naclkeypair.o lib/libnacl.a lib/randombytes.o
 fi
+
 gcc 	$CCFLAGS $DYFLAGS 	-o build/intf_dummy.o intf/intf_dummy.c
 gcc 	$CCFLAGS $DYFLAGS 	-o build/intf_tuntap.o intf/intf_tuntap.c
 gcc 	$CCFLAGS $DYFLAGS 	-o build/intf_udp.o intf/intf_udp.c
