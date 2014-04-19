3b7c5f41bafdbcc12bd9e37e56146dcd0dce39c1
Neil Alexander <neilalexander@frozenriver.net>
2014-04-19 18:11:10 +0100

    Add missing header files from previous commit

---
+++ proto.h
@@ -0,0 +1,35 @@
+//
+//  types.h
+//  Sigma type headers
+//
+//  Copyright (c) 2011, Neil Alexander T.
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with
+//  or without modification, are permitted provided that the following
+//  conditions are met:
+//
+//  - Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//  - Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+
+static int proto_init();
+static int proto_set(sigma_proto *instance, char* param, char* value);
+static int proto_reload(sigma_proto *instance);
+static int proto_decode(sigma_proto *instance, unsigned char* input, unsigned char* output, unsigned int len);
+static int proto_encode(sigma_proto *instance, unsigned char* input, unsigned char* output, unsigned int len);
